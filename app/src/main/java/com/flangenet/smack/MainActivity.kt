2bca1ac91763bb66b67e035dce3a6dc5feb7ed21
Wak <LeeTenerife@GMail.Com>
2020-03-30 13:37:21 +0100

    lesson 2

--- app/src/main/java/com/flangenet/smack/MainActivity.kt
+++ app/src/main/java/com/flangenet/smack/MainActivity.kt
@@ -2,6 +2,7 @@ package com.flangenet.smack
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -42,4 +43,11 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    fun loginBtnNavClicked(view: View) {}
+
+    fun addChannelClicked(view: View){}
+
+    fun sendMessageBtnClicked(view: View){}
+
 }
