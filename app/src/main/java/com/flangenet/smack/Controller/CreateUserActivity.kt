af46af0b6158486c8b42b0900a7ab18a0b705219
Wak <LeeTenerife@GMail.Com>
2020-04-01 13:14:30 +0100

    lesson 10

--- app/src/main/java/com/flangenet/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/flangenet/smack/Controller/CreateUserActivity.kt
@@ -51,8 +51,20 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View){
-        AuthService.registerUser(this,"W@W.com","123456"){ complete ->
-            if (complete){
+        val email = createEmailText.text.toString()
+        val password = createPasswordText.text.toString()
+        AuthService.registerUser(this, email, password) { registerSuccess ->
+            if (registerSuccess){
+                AuthService.loginUser(this, email, password) { loginSuccess ->
+                    if (loginSuccess){
+                        println(AuthService.authToken)
+                        println(AuthService.userEmail)
+
+
+                    }
+
+                }
+
                 //
             }
 
